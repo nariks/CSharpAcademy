aaadc254e282f952cdc8521f22822557fa138859
Narayan D <happycow@gmail.com>
2023-11-11 21:00:26 +1100

    Completed math game project and refactoring

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,51 +1,9 @@
-﻿int gameChoice = 0;
-
-int quizLength = 1;
-string operation = "";
-bool play = true;
-
-List<string> gameHistory = new List<string>();
+﻿List<string> gameHistory = new List<string>();
 gameHistory.Add("Question\t\t| Solution\t| Your Answer");
 
-
-void StartGame()
-{
-    do
-    {
-        int difficulty = 0;
-
-        do
-        {
-            DisplayMenu();
-            gameChoice = ReadUserInput();
-            operation = ChooseOperation(gameChoice);
-
-            if (operation == "")
-                goto ExitGame;
-        
-            do
-            {
-                difficulty = ChooseDiffictulty();
-                if (difficulty != 6)
-                    quizLength = ChooseQuizLength();
-            } while (difficulty!= 6 && quizLength == 6);            // Go back to difficulty menu
-            
-
-        } while (difficulty == 6);              // Go back to main menu
-
-        PlayGame(operation, difficulty, quizLength);
-        play = PlayAgain();
-
-    } while (play);
-}
-
-
-ExitGame:
-Console.WriteLine("\nGame Over !!!");
-Console.WriteLine("Press Enter to continue...");
-Console.ReadLine();
-
-    
+StartGame();
+Console.WriteLine("\n>> Game Over !!!");
+Console.WriteLine(">> Press Enter to continue...");    
 
 void DisplayMenu()
 {
@@ -57,6 +15,48 @@ void DisplayMenu()
     Console.WriteLine("4 - Division Game");
     Console.WriteLine("5 - View previous games");
     Console.WriteLine("6 - End Game\n");
+}
+
+void StartGame()
+{
+    do
+    {
+        int difficulty = 0;
+        int quizLength = 1;
+        string operation = "";
+
+        do
+        {
+            DisplayMenu();
+            int gameChoice = ReadUserInput();
+            operation = ChooseOperation(gameChoice);
+
+            if (operation == "")
+            {
+                return;
+            }
+            else if (operation == "show")
+            {
+                ShowHistory();
+                goto PlayAgain;
+            }
+                
+            do
+                {
+                    difficulty = ChooseDiffictulty();
+                    if (difficulty != 6)
+                        quizLength = ChooseQuizLength();
+                } while (difficulty!= 6 && quizLength == 6);            // Go back to difficulty menu
+                
+
+            } while (difficulty == 6);                                  // Go back to main menu
+
+        PlayGame(operation, difficulty, quizLength);
+
+        PlayAgain:
+        continue;
+
+    } while (PlayAgain());
 }
 
 string ChooseOperation(int gameChoice)
@@ -76,8 +76,7 @@ string ChooseOperation(int gameChoice)
             return "division";
 
         case 5:
-            ShowHistory();
-            return "";
+            return "show";
 
         case 6:
         default:
@@ -87,17 +86,16 @@ string ChooseOperation(int gameChoice)
 
 int ChooseDiffictulty()
 {
-    Console.Clear();
-    Console.WriteLine("Choose a difficulty level from 1 - 5");
-    Console.WriteLine("1 is Easiest and 5 is Hardest\n ");
-    Console.WriteLine("Or enter 6 to go back to main menu\n");
+    DisplayHeading("Difficulty Menu");
+    Console.WriteLine(">> Choose a difficulty level from 1 (Easy) - 5 (Hard)");
+    Console.WriteLine(">> Or enter 6 to go back to main menu\n");
     
     return ReadUserInput();
 }
 
 int ChooseQuizLength()
 {
-    Console.Clear();
+   DisplayHeading("Quiz Length");
     Console.WriteLine("Choose number of quiz questions: 1 - 5");
     Console.WriteLine("Or enter 6 to go back to main menu\n");
 
@@ -105,66 +103,33 @@ int ChooseQuizLength()
     
 }
 
-void PlayGame(string game, int difficulty = 1, int QuizLength = 1)
+void PlayGame(string game, int difficulty = 1, int quizLength = 1)
 {
     int score = 0;
-    string operation = "";
-    int operand1, operand2;
-    int result = 0;
-    const int stepSize = 21;
-    Random randomNumber = new Random();
+    int solution = 0;
     
+    Dictionary<string, string> operation = new Dictionary<string, string> 
+                                            { {"addition", "+"}, {"subtraction", "-"}, {"multiplication", "*"}, {"division", "/"} };
     
-    Console.Clear();
-    Console.WriteLine($"{game.ToUpper()} selected");
-    Console.WriteLine("--------------------");
+    string message = $"{game.ToUpper()} selected";
+    DisplayHeading(message);
     
     int minValue = SetDifficultyLevel(difficulty);
 
-    for(int i = 0; i < QuizLength; i++)
+    for(int i = 0; i < quizLength; i++)
     {
+        int[] operands = new int[2];
+        operands = GetRandomNumbers(minValue, game);
+        solution = CalculateResult(game, operands[0], operands[1]);
 
-        operand1 = randomNumber.Next(minValue, minValue + stepSize);
-        operand2 = randomNumber.Next(minValue, minValue + stepSize);
-
-        switch(game)
-        {
-            case "addition":
-                result = operand1 + operand2;
-                operation = "+";
-                break;
-
-            case "subtraction":
-                result = operand1 - operand2;
-                operation = "-";
-                break;
-
-            case "multiplication":
-                result = operand1 * operand2;
-                operation = "*";
-                break;
-
-            case "division":
-                while (operand1 % operand2 != 0) 
-                {
-                    operand2 = randomNumber.Next(1, minValue + stepSize);
-                } 
-                result = operand1 / operand2;
-                operation = "/";
-                break;
-
-            default:
-                break;
-        }
-
-        string question =  $"( {operand1} {operation} {operand2} )";
+        string question =  $"( {operands[0]} {operation[game]} {operands[1]} )";
         Console.WriteLine($"Qn{i+1}: What is {question} ?");
         
         int playerAnswer = GetPlayerSolution();
         
-        gameHistory.Add(question + "\t\t| " + result + "\t\t| " + playerAnswer);
+        gameHistory.Add(question + "\t\t| " + solution + "\t\t| " + playerAnswer);
 
-        if (playerAnswer == result)
+        if (playerAnswer == solution)
         {
             Console.WriteLine("That's correct. \u2705\n");  
             score++;  
@@ -175,7 +140,7 @@ void PlayGame(string game, int difficulty = 1, int QuizLength = 1)
         }
             
     }
-    Console.WriteLine($"Game Over ! You scored {score} points\n");
+    Console.WriteLine($"Game Over ! You scored {score} out of {quizLength} points\n");
 }
 
 int SetDifficultyLevel (int difficulty)
@@ -205,7 +170,7 @@ int SetDifficultyLevel (int difficulty)
 
 int ReadUserInput()
 {
-    string readResult = "";
+    string? readResult = "";
     int userChoice = 0;
     bool validEntry = false;
 
@@ -227,7 +192,7 @@ int ReadUserInput()
 bool PlayAgain()
 {
     Console.WriteLine("Would you like to play again (Y/N)?");
-    string readResult = Console.ReadLine(); 
+    string? readResult = Console.ReadLine(); 
     if (readResult != null)
         return (readResult.ToLower() == "y");
     else
@@ -241,7 +206,7 @@ int GetPlayerSolution()
         
     do
     {
-        string readResult = Console.ReadLine();
+        string? readResult = Console.ReadLine();
         validEntry = int.TryParse(readResult, out answer);
         if (!validEntry)
             Console.WriteLine("Only numbers can be accepted as valid answer.");
@@ -261,10 +226,57 @@ void ShowHistory()
         Console.WriteLine("No Games played yet!");
         return;
     }
-        
-
+    
     foreach (string game in gameHistory)
     {
         Console.WriteLine(game);
     }
+
+    Console.WriteLine("\nPress Enter to continue");
+    Console.ReadLine();
+}
+
+void DisplayHeading (string message)
+{
+    Console.Clear();
+    Console.WriteLine(message);
+    Console.WriteLine("--------------------\n");
+}
+
+int CalculateResult(string game, int operand1, int operand2)
+{
+    switch(game)
+        {
+            case "addition":
+                return operand1 + operand2;
+
+            case "subtraction":
+                return operand1 - operand2;
+                
+            case "multiplication":
+                return operand1 * operand2;
+
+            case "division":
+                return operand1 / operand2;
+            
+            default:
+                return 0;
+        }
+}
+
+int[] GetRandomNumbers( int minValue, string game)
+{
+    const int stepSize = 21;
+    Random randomNumber = new Random();
+    int[] operands = new int[2];
+
+    operands[0] = randomNumber.Next(minValue, minValue + stepSize);
+    operands[1] = randomNumber.Next(minValue, minValue + stepSize);
+    while (game == "division" && (operands[0] % operands[1] != 0)) 
+    {
+        operands[1] = randomNumber.Next(1, minValue + stepSize);
+    }
+
+    return operands; 
+
 }
